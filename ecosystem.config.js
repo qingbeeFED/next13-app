dfd975ca536f20ed2e7a705e97e7a06035a12f1a
singcl <1276837689@qq.com>
2023-05-14 13:03:18 +0800

    ci(build): build update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,40 @@
 module.exports = {
-  apps : [{
-    "name": "pm2_next13",
-    "script": 'node_modules/next/dist/bin/next',
-    "args": "start",
-    "cwd": "./",
-    "instances": "2",
-    "exec_mode": "cluster",
-    // "log_type": "json",
-    "error_file": "./logs/pm2_next13-error.log",
-    "out_file": "./logs/pm2_next13-out.log",
-    // "log_file": ""
-  }]
+  apps: [
+    {
+      name: "pm2_next13",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "./",
+      instances: "2",
+      exec_mode: "cluster",
+      // "log_type": "json",
+      error_file: "./logs/pm2_next13-error.log",
+      out_file: "./logs/pm2_next13-out.log",
+      // "log_file": ""
+      env: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "pm2_next13_development",
+      script: "node_modules/next/dist/bin/next",
+      args: "dev",
+      cwd: "./",
+      instances: "2",
+      exec_mode: "cluster",
+      // "log_type": "json",
+      error_file: "./logs/pm2_next13-error.log",
+      out_file: "./logs/pm2_next13-out.log",
+      // "log_file": ""
+      env: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
 };
