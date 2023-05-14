cccebfa2922315cb42ac8931d195e5dc89375567
singcl <1276837689@qq.com>
2023-05-14 15:39:12 +0800

    feat(ci): ci build logger

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import logger from "@/logger/logger";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // Logging to pino-logflare
+
+  logger.info(
+    { name: "singcl" },
+    "Handled response. Logged with pino-logflare."
+  );
+  res.status(200).json({ name: "singcl" });
 }
